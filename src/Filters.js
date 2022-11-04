8f44de112403b29275c95273ac481a9ac94a7def
Mike Boyle <mikesboyle@gmail.com>
2022-11-04 15:20:05 -0400

    highlight selected filters

--- src/Filters.js
+++ src/Filters.js
@@ -4,6 +4,8 @@ import { clickFilter } from './app/actions';
 const Filters = () => {
   const dispatch = useDispatch();
   const filters = useSelector((state) => state.filters);
+  const selectedFilters = useSelector((state) => state.selectedFilters);
+
   const handleFilterClick = (filter) => {
     dispatch(clickFilter(filter));
   };
@@ -11,6 +13,9 @@ const Filters = () => {
     <div>
       {filters.map((filter) => (
         <button
+          className={`Filter-button${
+            selectedFilters.includes(filter) ? '--selected' : ''
+          }`}
           key={filter}
           value={filter}
           onClick={() => handleFilterClick(filter)}
